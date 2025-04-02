1d962266d413bc8af921515a6c1add78029e2325
kaushal-33 <varmakaushal33@gmail.com>
2025-04-02 12:30:13 +0530

    added to Add to cart project in index page, added checkout box in add to cart project

--- Add-to-cart/assets/js/index.js
+++ Add-to-cart/assets/js/index.js
@@ -1,70 +1,82 @@
 let productArr = [{
+    id: 1,
     name: "valeo",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/valeo_404525_clutch_set_clutch_pressure_plate_ford_figo_fiesta_ikon_1_4_ltr_diesel_.jpg",
     discription: "clutch set ford figo/fiesta/ikon 1.4ltr diesel",
     rating: 4,
     price: 6600
 }, {
+    id: 2,
     name: "bosch",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/bosch_f002h600368f8_brake_pad_front_swift_swift_dzire_ritz_diesel_.jpg",
     discription: "brake pad front swift/swift dzire/ ritz diesel",
     rating: 4,
     price: 1000
 }, {
+    id: 3,
     name: "bosch",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/bosch_0986ab70048f8_brake_fluid_dot_3_500_ml_.jpg",
     discription: "brake fluid DOT 3 (500ml)",
     rating: 3.5,
     price: 500
 }, {
+    id: 4,
     name: "autogold",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/autogold_tail_light_lamp_assembly_scorpio_type_3_s2_s4_s8_s10_white_left_.jpg",
     discription: "tail light lamp assembly scorpio type 3/s2/s4/s8/s10 (white)",
     rating: 4.5,
     price: 2500
 }, {
+    id: 5,
     name: "far",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/far_vision_side_door_mirror_etios_etios_liva_manual_type_2_left_.jpg",
     discription: "vision side door mirror etios/ etios liva (manual)",
     rating: 4,
     price: 1850
 }, {
+    id: 6,
     name: "bosch",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/300x/17f82f742ffe127f42dca9de82fb58b1/i/m/bosch_0986ab8127e7a_wheel_cylinder_assembly_hyundai_i10_lhs_.jpg",
     discription: "wheel cylinder assembly hyundai i10 LHS",
     rating: 5,
     price: 3000
 }, {
+    id: 7,
     name: "valeo",
     image: "https://www.partsbigboss.in/media/catalog/product/cache/1/small_image/250x/17f82f742ffe127f42dca9de82fb58b1/i/m/valeo_049111_head_light_lamp_assembly_nissan_sunny_type_1_right.jpg",
     discription: "head light lamp assembly nissan sunny",
     rating: 4,
     price: 5000
 }, {
+    id: 8,
     name: "spenz",
     image: "https://opencart.dostguru.com/AS01/autospeed_02/image/cache/catalog/product/16-288x288.jpg",
     discription: "car seat cushion car decor",
     rating: 4,
     price: 12000
 }, {
+    id: 9,
     name: "sonic",
     image: "https://opencart.dostguru.com/AS01/autospeed_02/image/cache/catalog/product/30-800x800.jpg",
     discription: "car pump washing machine",
     rating: 3.5,
     price: 2000
 }, {
+    id: 10,
     name: "audi",
     image: "https://opencart.dostguru.com/AS01/autospeed_02/image/cache/catalog/product/11-288x288.jpg",
     discription: "steering wheel",
     rating: 5,
     price: 15000
 }, {
+    id: 11,
     name: "DC",
     image: "https://opencart.dostguru.com/AS01/autospeed_02/image/cache/catalog/product/27-288x288.jpg",
     discription: "car disc brake",
     rating: 4.5,
     price: 7500
 }, {
+    id: 12,
     name: "toyota",
     image: "https://opencart.dostguru.com/AS01/autospeed_02/image/cache/catalog/product/19-288x288.jpg",
     discription: "engine assembly 1.4ltr diesel",
@@ -73,11 +85,49 @@ let productArr = [{
 },]
 
 let productContainer = document.getElementById("car-parts")
+let cartArr = JSON.parse(localStorage.getItem("cartArray")) || [];
+let counter = document.getElementById("item-counter")
+counter.innerHTML = cartArr.length
+function addToCart(productID) {
+    let item = productArr.find((obj) => { return obj.id == productID })
+    let itemIndex = cartArr.findIndex((obj) => { return obj.id == productID })
+    if (itemIndex != -1) {
+        cartArr[itemIndex].quantity++;
+    } else {
+        item.quantity = 1
+        cartArr.push(item)
+    }
+    localStorage.setItem("cartArray", JSON.stringify(cartArr))
+    counter.innerHTML = cartArr.length
+    Swal.fire({
+        position: "top-end",
+        toast: true,
+        icon: "success",
+        title: "Item added to cart.",
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
+
+function getStars(num) {
+    let starString = "";
+    let i = 0;
+
+    for (i = 1; i <= num; i++) {
+        starString += `<i class="ri-star-s-fill"></i>`;
+    }
+
+    if (num - (i - 1) > 0) {
+        starString += `<i class="ri-star-half-line"></i>`;
+    }
+
+    return starString;
+}
 
 productArr.forEach((product, idx) => {
     productContainer.innerHTML += `
     <div class="col-lg-3 col-md-6">
-        <div class="border shadow-sm p-3 card">
+        <div class="border border-2 shadow-sm p-3 card">
             <div>
                 <img src="${product.image}" alt="${product.name}" width="100%" class="">
             </div>
@@ -85,11 +135,7 @@ productArr.forEach((product, idx) => {
             <p class="mb-1 fs-sm text-capitalize text-secondary">${product.discription}</p>
             <div class="d-flex justify-content-between align-items-center">
                 <span class="text-warning d-block">
-                     <i class="ri-star-s-fill"></i>
-                     <i class="ri-star-s-fill"></i>
-                     <i class="ri-star-s-fill"></i>
-                     <i class="ri-star-s-fill"></i>
-                     <i class="ri-star-s-line"></i>
+                    ${getStars(product.rating)}
                 </span>
                 <p class="m-0 fs-5">
                     <i class="ri-money-rupee-circle-line"></i>
@@ -99,8 +145,8 @@ productArr.forEach((product, idx) => {
                 </p>
             </div>
             <div class="mt-3 text-md-start text-center">
-                <button class="btn border text-capitalize fs-sm" id="buy-now">buy now</button>
-                <button class="btn border text-capitalize fs-sm" id="to-cart">add to cart</button>
+                <button class="btn border text-capitalize fs-sm buy-now">buy now</button>
+                <button class="btn border text-capitalize fs-sm to-cart" onclick="addToCart(${product.id})">add to cart</button>
             </div>
         </div>
     </div>`
